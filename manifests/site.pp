cc3fd638c6dab88beeed2ecf00af3a81c3224f7f
EC2 Default User <ec2-user@ip-10-0-5-251.us-west-2.compute.internal>
2016-07-11 20:18:53 +0000

    Added sshd module to amazon

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node ip-10-0-5-71 {
     cron { "puppet update":
         command => "cd /etc/puppet && git pull -q origin master",
         user    => root,
-        minute  => "*/5",
+        minute  => "*/1",
     }
 
 }
@@ -15,5 +15,5 @@ node ip-10-0-5-182 {
 }
 
 node ip-10-0-5-251 {
-
+    include "sshd"
 }
