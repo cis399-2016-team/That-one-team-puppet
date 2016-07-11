1e6185f00150fcc22754407ebae5710cbea94344
EC2 Default User <ec2-user@ip-10-0-5-251.us-west-2.compute.internal>
2016-07-11 20:33:21 +0000

    init edit

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -1,25 +1,36 @@
 class sshd {
-    package {
-        "ssh": ensure => installed
-    }
-    
-    service { "sshd": 
-        enable => true,
-        ensure => running,
-        hasrestart => true,
-        subscribe => File["/etc/ssh/sshd_config"],
-        require => Package["openssh-server"]
-    }
+package {
+"openssh-server": ensure => installed;
+}
 
-    ssh_authorized_key { "eewing_key":
-        user => "ubuntu",
-        key => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRXDK0AELCMQXqQbkCU97JXBVUBPKrtEXTym/tZWGoc8cfXDBNOk1ooRUm07D8tc7zHs3zHibPD8UdeTCwd+jtqhvwamD9dLil8dlvCp03go65/3TrxocWxa2rDlx4uPqwWfM+Y/tKvybCGmg73r1EZwZ+WFooL/b+/0oOX+RY+UyMUX6f7/+dicFyMzr07E9aVHLwTdoR3ieHcEMU4OdeqICAWct2K6kdOpHonib0c55D6vwjNTkPzqNqhXj2bldaV3w6mr1XQP4SD4A7vrWLo7U8nUzkILKeF05/VvaU0eXEcQysv56Et07ZtLOb8nAYKpyqVzpbRxbO1L3U9Jx1 Quentillionaire@FINN"
-    }
+file { "/etc/ssh/sshd_config":
+source  => [
+# from modules/smartd/files/$hostname/smartd.conf
+"puppet:///modules/sshd/$hostname/sshd.conf",
+# from modules/smartd/files/smartd.conf
+"puppet:///modules/sshd/sshd.conf",
+],
+mode    => 444,
+owner   => root,
+group   => root,
+}
 
-    ssh_authorized_key { "mike_key":
-        user => "ubuntu",
-        key => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW6KxhZQURLzasQlBObJxCMYmAZgezXPC0buM3e5h2GaTdpOAGAgHM4CVqUnNjIEEQX0lWAOju+JLQma4QzFaCUNcTJXvnrTK+XqDr3GkdhrQdGKWcS/1BHN1mrHwGymOmL7TiVJyhDRd/dhAEkOH3nukGOkErVJmsnc30PnlOtYReEZYGkzFNW9mNBoQJupU/1huiIBLjXDlVUaqDn5CQ03WFiHaK1O6V+Pg+AJ8rNXhFesF/Mlmvpal3G1yqREDHFvr4skQdBrpIxFXhDCi2s1M7kghKtlueYqFe4EmTVZ8r5C50NZiqoRVpEUXxTKOqQodL/UaR0aWo9U2QqWJx panda
-"
-    }
+service { "sshd":
+# automatically start at boot time
+enable     => true,
+# restart service if it is not running
+ensure     => running,
+# "service smartd status" returns useful service status info
+hasstatus  => true,
+# "service smartd restart" can restart service
+hasrestart => true,
+# package and configuration must be present for service
+require    => [ Package["openssh-server"],
+        File["/etc/ssh/sshd_config"] ],
+# changes to configuration cause service restart
+subscribe  => File["/etc/ssh/sshd_config"],
+
+}
+
 
 }
