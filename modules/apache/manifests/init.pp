a637b0458951371b22f1c00e4fe0d4e5128d5ee1
EC2 Default User <ec2-user@ip-10-0-5-251.us-west-2.compute.internal>
2016-07-18 01:58:37 +0000

    removed inherits clause

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,11 +1,17 @@
 class apache {
 
-    $apachepkg = $::apache::params::apachepkg,
+    if $::osfamily == 'RedHat' {
+        $apachepkg = 'httpd'
+    elseif $::osfamily == 'Ubuntu' {
+        $apachepkg = 'apache2'
+    } else {
+        print "Apache is not supported on this distro."
+    }
 
     package { 'apache':
         name   => $apachepkg,
         ensure => installed;
     }
 
-} inherits ::apache::params
+}
 
--- modules/apache/manifests/params.pp
+++ modules/apache/manifests/params.pp
@@ -1,11 +1,4 @@
 class apache::params {
 
-    if $::osfamily == 'RedHat' {
-        $apachepkg = 'httpd'
-    elseif $::osfamily == 'Ubuntu' {
-        $apachepkg = 'apache2'
-    } else {
-        print "Apache is not supported on this distro."
-    }
 
 }
