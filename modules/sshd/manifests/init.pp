84c264cab454a9547045771a68acbde8a6c553ea
EC2 Default User <ec2-user@ip-10-0-5-251.us-west-2.compute.internal>
2016-07-18 02:30:15 +0000

    Added ubuntu to supported ssh daemon settings

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -14,31 +14,34 @@ mode    => 644,
 owner   => root,
 group   => root,
 }
-
-service { "sshd":
-# automatically start at boot time
-enable     => true,
-# restart service if it is not running
-ensure     => running,
-# "service sshd status" returns useful service status info
-hasstatus  => true,
-# "service sshd restart" can restart service
-hasrestart => true,
-# package and configuration must be present for service
-require    => [ Package["openssh-server"],
-        File["/etc/ssh/sshd_config"] ],
-# changes to configuration cause service restart
-subscribe  => File["/etc/ssh/sshd_config"],
-
-}
-
     if $::operatingsystem == 'Amazon' or $::operatingsystem == 'RedHat' {
         $username = 'ec2-user'
+        $sshservice = 'sshd'
     } elsif $::operatingsystem == 'Ubuntu' {
         $username = 'ubuntu'
+        $sshservice = 'ssh'
     } else {
         notify {"username could not be set":}
     }
+
+    service { "sshd":
+        name       => $sshservice,
+            # automatically start at boot time
+        enable     => true,
+            # restart service if it is not running
+        ensure     => running,
+            # "service sshd status" returns useful service status info
+        hasstatus  => true,
+            # "service sshd restart" can restart service
+        hasrestart => true,
+            # package and configuration must be present for service
+        require    => [ Package["openssh-server"],
+            File["/etc/ssh/sshd_config"] ],
+            # changes to configuration cause service restart
+        subscribe  => File["/etc/ssh/sshd_config"],
+
+}
+
 
     ssh_authorized_key { "eewing-key-pair":
         user => $username,
