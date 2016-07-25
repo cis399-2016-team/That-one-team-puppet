308cd07f2f6a81d901face0b704e6759012ca893
Ubuntu <ubuntu@ip-10-0-5-71.us-west-2.compute.internal>
2016-07-25 03:19:25 +0000

    Update new users

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -25,11 +25,11 @@ define useradd($name, $uid, $ssh_key_name, $ssh_key) {
 class users {
 
 
-useradd { "eewing": 
-	name => "eewing", 
+useradd { "chenders": 
+	name => "chenders", 
 	uid => 1001,
-	ssh_key_name => "eewing@DESKTOP-VLHI5BD",
-	ssh_key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC3AaBSn+a5j9R7XS2VlyKiaptQaWyeJdgq71wTA8PUNtzzrE6Djv955CQvbjurLKEXPBjrzQdyiotqPfgMD8hBBcBUuT04edDf/ccgaYPYU77S+utGE4vwUdb3YNuUTJFErCnR4FVW2sXGlF6LIMb6crPVC4WHvzsXrKuuRBwo9SJT1VpjZ1IKaVRzcMncspr+ZLP7maq3e8U4GLas3owD0qnrxB3tmC1l4fJ0wOyv9TDWCHYY1iMs0+XPPQa2vCUPGf1qL/yWJo+6L77K+n6J7D09ovaWYxP8DFYYo7PvxraOK1xoQV9Ysm4NKHa0awDO2oqVy7wvan8CTCd46gaT"
+	ssh_key_name => "chenders-key-pair-oregon",
+	ssh_key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCWR1HbA7kQtEHE2qHTQnuTNsEGQtz8cMg5HID4WaMwOdNKGYR4ARmXHhI6Lpn2/kQ97ay2j1M1tYZhJSsglqrsGRY+gxEyr3Yp5Aq4KVLqYEnvHV5cfRbpekD3zGULH2dri8C/c/Z8ASBNHmm3deLz1IiB2IetTZ3fCS3VAqvlEF6WIbXw13j5kGGHbPG/TFevdKXwQTAK75P1LaNx00vjTHPP03/nmwRItl3lLuYS8RXyrOtmJR/oxCWqu+WZCBV5Dz37idScZ/kwrbjaDNTBrydC5lArn2x6m2r9JIn5cjI7hf3FZAfOkXSivxBTlpZ3hYW41XvHewLo3kKVIamX"
 }
 
 }
