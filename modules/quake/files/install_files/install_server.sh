acd0ae9b5fbe4647f8d5eb559213462fc55fafe1
Ubuntu <ubuntu@ip-10-0-5-71.us-west-2.compute.internal>
2016-08-12 23:18:57 +0000

    Fixed formatting

--- modules/quake/files/install_files/install_server.sh
+++ modules/quake/files/install_files/install_server.sh
@@ -1,2 +1,4 @@
+echo "Compiling server"
 sh server_compile.sh
+echo "Installing server files"
 cp pak0.pk3 /home/quake/ioquake3/baseq3/
--- modules/quake/files/install_files/server_compile.sh
+++ modules/quake/files/install_files/server_compile.sh
@@ -9,13 +9,14 @@ export COPYDIR="/home/quake/ioquake3"
 IOQ3REMOTE="https://github.com/ioquake/ioq3.git"
 IOQ3LOCAL="/tmp/ioquake3compile"
 JOPTS="-j2" 
-echo "This process requires you to have the following installed through your distribution:
- make
- git
- and all of the ioquake3 dependencies necessary for an ioquake3 server.
- If you do not have the necessary dependencies this script will bail out.
- Please post a message to http://discourse.ioquake.org/ asking for help and include whatever error messages you received during the compile phase.
- Please edit this script. Inside you will find a COPYDIR variable you can alter to change where ioquake3 will be installed to."
+#echo "This process requires you to have the following installed through your distribution:
+# make
+# git
+# and all of the ioquake3 dependencies necessary for an ioquake3 server.
+# If you do not have the necessary dependencies this script will bail out.
+# Please post a message to http://discourse.ioquake.org/ asking for help and include whatever error messages you received during the compile phase.
+# Please edit this script. Inside you will find a COPYDIR variable you can alter to change where ioquake3 will be installed to."
+echo "Compiling server now.";
 if  [ -x "$(command -v git)" ] && [ -x "$(command -v make)" ] ; then
         git clone $IOQ3REMOTE $IOQ3LOCAL && cd $IOQ3LOCAL && make $JOPTS && make copyfiles && cd $localPATH && rm -rf $IOQ3LOCAL
 fi
