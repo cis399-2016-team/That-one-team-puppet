1edcb66306e3a6eeb199760eb3fcbf29d96ab92c
Ubuntu <ubuntu@ip-10-0-5-71.us-west-2.compute.internal>
2016-08-12 23:54:02 +0000

    Changed pak file location

--- modules/quake/files/install_files/install_server.sh
+++ modules/quake/files/install_files/install_server.sh
@@ -4,5 +4,5 @@ echo "Compiling server"
 sh /home/quake/quake_install/server_compile.sh
 
 echo "Installing server files"
-wget ftp://213.141.144.202/pub/Soft/4Android/quake3/baseq3/pak0.pk3 /home/quake/ioquake3/baseq3/pak0.pk3
-#cp pak0.pk3 /home/quake/ioquake3/baseq3/
+#wget ftp://213.141.144.202/pub/Soft/4Android/quake3/baseq3/pak0.pk3 /home/quake/ioquake3/baseq3/pak0.pk3
+cp /home/quake/quake_install/pak0.pk3 /home/quake/ioquake3/baseq3/
