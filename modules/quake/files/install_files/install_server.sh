d9beec030b6e20315fc2351e6ca71c6700cab2b3
Ubuntu <ubuntu@ip-10-0-5-71.us-west-2.compute.internal>
2016-08-13 00:17:15 +0000

    Change pak file allocation source

--- modules/quake/files/install_files/install_server.sh
+++ modules/quake/files/install_files/install_server.sh
@@ -4,5 +4,6 @@ echo "Compiling server"
 sh /home/quake/quake_install/server_compile.sh
 
 echo "Installing server files"
+wget http://files.anitalink.com/gamecache/quake3/baseq3/pak0.pk3 /home/quake/ioquake3/baseq3/pak0.pk3
 #wget ftp://213.141.144.202/pub/Soft/4Android/quake3/baseq3/pak0.pk3 /home/quake/ioquake3/baseq3/pak0.pk3
-cp /home/quake/quake_install/pak0.pk3 /home/quake/ioquake3/baseq3/
+#cp /home/quake/quake_install/pak0.pk3 /home/quake/ioquake3/baseq3/
