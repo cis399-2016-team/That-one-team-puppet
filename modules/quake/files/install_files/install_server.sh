12ba2c88e745cda18d8c594155800af55c293c8e
Ubuntu <ubuntu@ip-10-0-5-71.us-west-2.compute.internal>
2016-08-13 01:24:47 +0000

    added worker functions

--- modules/quake/files/install_files/install_server.sh
+++ modules/quake/files/install_files/install_server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cd /home/quake/quake_install && wget http://ioquake3.org/files/1.36/installer/ioquake3-1.36-7.1.x86_64.run -O ioquake_install.run
+wget http://ioquake3.org/files/1.36/installer/ioquake3-1.36-7.1.x86_64.run -O ioquake_install.run
 
-cd /home/quake/quake_install && wget http://ioquake3.org/files/1.36/data/ioquake3-q3a-1.32-9.run -O ioquake_patch.run
+wget http://ioquake3.org/files/1.36/data/ioquake3-q3a-1.32-9.run -O ioquake_patch.run
 
-cd /home/quake/quake_install && wget http://files.anitalink.com/gamecache/quake3/baseq3/pak0.pk3
+wget http://files.anitalink.com/gamecache/quake3/baseq3/pak0.pk3
 
-#sh ioquake_install.run
-#sh ioquake_patch.run
+sh ioquake_install.run
+sh ioquake_patch.run
+cp pak0.pk3 /usr/local/games/ioquake3/baseq3/
