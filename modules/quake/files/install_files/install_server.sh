41bd44201f5eeed2f5622385ccc7925c9f50e8ad
Mike Harris <mike.a.harris@outlook.com>
2016-09-07 17:41:57 -0700

    opena arena

--- modules/quake/files/install_files/install_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Compiling server"
-sh /home/quake/quake_install/server_compile.sh
-
-echo "Installing server files"
-#wget ftp://213.141.144.202/pub/Soft/4Android/quake3/baseq3/pak0.pk3 /home/quake/ioquake3/baseq3/pak0.pk3
-cp /home/quake/quake_install/pak0.pk3 /home/quake/ioquake3/baseq3/
